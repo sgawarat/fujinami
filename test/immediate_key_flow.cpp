5ab25a7b1350c81f61a66516592568affc922456
sugawara <sgawarat@gmail.com>
2017-03-18 00:37:01 +0900

    Update ImmediateKeyFlow test

--- test/immediate_key_flow.cpp
+++ test/immediate_key_flow.cpp
@@ -1,40 +1,91 @@
 ﻿#include <catch.hpp>
 #include <fujinami/buffering/flow/immediate.hpp>
 
+using namespace std::chrono_literals;
 using namespace fujinami;
 using namespace fujinami::buffering;
 
-TEST_CASE("ImmediateKeyFlow", "[fujinami][flow]") {
-  const Key key = to_key(42);
-  const KeyRole key_role = KeyRole::TRIGGER;
-  KeyboardConfig config;
-  auto unmapped_layout = config.create_layout("unmapped");
-  auto mapped_layout = config.create_layout("mapped");
-  mapped_layout->create_flow(key, FlowType::IMMEDIATE);
-  mapped_layout->create_mapping({&key, 1}, {&key_role, 1}, Command{});
+#define REQUIRE_STATE(t, m, dc)\
+  do {\
+    const size_t size = state.events().size();\
+    REQUIRE(flow.reset(state) == FlowResult::DONE);\
+    REQUIRE(size == state.events().size() + 1);\
+    REQUIRE(state.trigger_keyset() == t);\
+    REQUIRE(state.modifier_keyset() == m);\
+    REQUIRE(state.dontcare_keyset() == dc);\
+  } while(false)
 
-  SECTION("reset with unmapped layout") {
-    State state;
-    state.reset(unmapped_layout);
-    state.push_event(KeyPressEvent{Clock::now(), key});
+TEST_CASE("ImmediateKeyFlow", "[fujinami][buffering][flow]") {
+  const Key trigger_key = to_key(1);
+  const Key modifier_key = to_key(21);
+  const Key unmapped_key = to_key(42);
+  const KeyRole trigger_key_role = KeyRole::TRIGGER;
+  const KeyRole modifier_key_role = KeyRole::MODIFIER;
+  const Keyset none_keyset{};
+  const Keyset trigger_keyset{trigger_key};
+  const Keyset modifier_keyset{modifier_key};
+  const Keyset all_keyset{trigger_key, modifier_key};
 
+  auto config = std::make_shared<KeyboardConfig>();
+  auto layout = config->create_layout("ImmediateKeyFlowTest");
+  config->set_default_layout(layout);
+  layout->create_flow(trigger_key, FlowType::IMMEDIATE);
+  layout->create_flow(modifier_key, FlowType::IMMEDIATE);
+  layout->create_mapping({trigger_key}, {trigger_key_role}, Command{});
+  layout->create_mapping({modifier_key}, {modifier_key_role}, Command{});
+  layout->create_mapping({trigger_key, modifier_key},
+                         {trigger_key_role, modifier_key_role},
+                         Command{});
+
+  SECTION("press unmapped key") {
     ImmediateKeyFlow flow;
-    REQUIRE(flow.reset(state) == FlowResult::DONE);
-    REQUIRE(state.events().empty());
-    REQUIRE(state.trigger_keyset() == Keyset{});
-    REQUIRE(state.dontcare_keyset() == Keyset{key});
+    State state;
+    state.reset(config);
+
+    // 0: Up Up Up
+    state.push_event(KeyPressEvent{Clock::time_point(0ms), unmapped_key});
+    state.push_event(KeyPressEvent{Clock::time_point(2ms), unmapped_key});
+    state.push_event(KeyPressEvent{Clock::time_point(3ms), unmapped_key});
+    REQUIRE_STATE(none_keyset, none_keyset, Keyset{unmapped_key});
+    REQUIRE_STATE(none_keyset, none_keyset, Keyset{unmapped_key});
+    REQUIRE_STATE(none_keyset, none_keyset, Keyset{unmapped_key});
   }
 
-  SECTION("reset with mapped layout") {
-    State state;
-    state.reset(mapped_layout);
-    state.push_event(KeyPressEvent{Clock::now(), key});
-
+  SECTION("press trigger key") {
     ImmediateKeyFlow flow;
-    REQUIRE(flow.reset(state) == FlowResult::DONE);
-    REQUIRE(state.events().empty());
-    REQUIRE(state.trigger_keyset() == Keyset{key});
-    REQUIRE(state.dontcare_keyset() == Keyset{key});
+    State state;
+
+    // 0: Tp Tp Tp
+    state.reset(config);
+    state.push_event(KeyPressEvent{Clock::time_point(0ms), trigger_key});
+    state.push_event(KeyPressEvent{Clock::time_point(2ms), trigger_key});
+    state.push_event(KeyPressEvent{Clock::time_point(3ms), trigger_key});
+    REQUIRE_STATE(trigger_keyset, none_keyset, trigger_keyset);
+    REQUIRE_STATE(trigger_keyset, none_keyset, trigger_keyset);
+    REQUIRE_STATE(trigger_keyset, none_keyset, trigger_keyset);
+
+    // 1: Mp Mp Mp
+    state.reset(config);
+    state.push_event(KeyPressEvent{Clock::time_point(100ms), modifier_key});
+    state.push_event(KeyPressEvent{Clock::time_point(102ms), modifier_key});
+    state.push_event(KeyPressEvent{Clock::time_point(103ms), modifier_key});
+    REQUIRE_STATE(none_keyset, modifier_keyset, modifier_keyset);
+    REQUIRE_STATE(none_keyset, modifier_keyset, modifier_keyset);
+    REQUIRE_STATE(none_keyset, modifier_keyset, modifier_keyset);
+
+    // 2: Tp Mp
+    state.reset(config);
+    state.push_event(KeyPressEvent{Clock::time_point(200ms), trigger_key});
+    state.push_event(KeyPressEvent{Clock::time_point(201ms), modifier_key});
+    REQUIRE_STATE(trigger_keyset, none_keyset, trigger_keyset);
+    REQUIRE_STATE(none_keyset, modifier_keyset, all_keyset);
+
+    // 3: Mp Tp
+    state.reset(config);
+    state.push_event(KeyPressEvent{Clock::time_point(300ms), modifier_key});
+    state.push_event(KeyPressEvent{Clock::time_point(301ms), trigger_key});
+    REQUIRE_STATE(none_keyset, modifier_keyset, modifier_keyset);
+    REQUIRE_STATE(trigger_keyset, modifier_keyset, all_keyset);
   }
 
   SECTION("is_idle") {
--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,8 @@
-﻿#define CATCH_CONFIG_MAIN
+﻿#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+int main(int argc, char* argv[]) {
+  const int result = Catch::Session().run(argc, argv);
+  system("PAUSE");
+  return result < 0xff ? result : 0xff;
+}
